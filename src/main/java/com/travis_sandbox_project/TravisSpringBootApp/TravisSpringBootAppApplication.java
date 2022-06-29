62363beab10e21642eb306e2038dd4ccdcd47d76
tdutchover <Travis.Dutchover@gmail.com>
2022-06-29 15:41:20 -0700

    Add files via upload

---
+++ src/main/java/com/travis_sandbox_project/TravisSpringBootApp/TravisSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.travis_sandbox_project.TravisSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travis_sandbox_project/TravisSpringBootApp/TravisSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travis_sandbox_project.TravisSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
