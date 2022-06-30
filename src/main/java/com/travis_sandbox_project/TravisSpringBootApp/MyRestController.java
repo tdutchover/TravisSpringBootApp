bb6c40b876e90e15aa4834ed3467bb643c2cc266
Travis Dutchover <travis.dutchover@gmail.com>
2022-06-29 23:49:34 -0700

    initial commit

---
+++ src/main/java/com/travis_sandbox_project/TravisSpringBootApp/MyRestController.java
@@ -0,0 +1,12 @@
+package com.travis_sandbox_project.TravisSpringBootApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello Travis!";
+    }
+}
